f2fe347c6b3b939df59f6f2a54c5f2e40413476b
Morten Raeburn <morten@raeburn.dev>
2022-12-10 20:20:07 +0100

    Fix interpolation

--- src/lagrange.rs
+++ src/lagrange.rs
@@ -1,20 +1,21 @@
-use num::{BigInt, One, Zero};
+use num::{BigInt, One, Zero, BigRational};
 
 #[derive(Clone)]
 pub struct Share(pub usize, pub BigInt);
 
-fn lagrange(shares: &[Share], j: usize, x: usize) -> BigInt {
-    let mut res = BigInt::one();
-    let xj = shares.get(j).unwrap().0;
+fn lagrange(shares: &[Share], j: usize, x: usize) -> BigRational {
+    let mut res = BigRational::one();
+    let xj = shares[j].0;
 
     for share in shares.iter() {
         let xm = share.0;
-
-        if xj == xm {
+        
+        if xm == xj {
             continue;
         }
 
-        res *= x as isize - xm as isize / (xm as isize - xj as isize);
+        res *= BigInt::from(x as isize - xm as isize);
+        res /= BigInt::from(xj as isize - xm as isize);
     }
 
     res
@@ -24,14 +25,14 @@ pub fn interpolate(shares: &[Share]) -> impl Fn(usize) -> BigInt {
     let shares = shares.to_vec();
 
     move |x: usize| {
-        let mut res = BigInt::zero();
+        let mut res = BigRational::zero();
 
         for (j, share) in shares.iter().enumerate() {
             let yj = &share.1;
 
-            res += yj * lagrange(&shares, j, x);
+            res += lagrange(&shares, j, x) * yj;
         }
 
-        res
+        res.to_integer()
     }
 }
